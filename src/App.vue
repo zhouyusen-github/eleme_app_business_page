29ff4fd45b7a31659c099e6d7535ea2eaf3fd5b8
Windcolour <1015357985@qq.com>
2020-02-01 18:33:30 +0800

    header.vue:添加三个区块占位置 之所以能显示在网页的原因：main.js中把App.vue挂载在body

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-
+    <div class="header">
+      i am header
+    </div>
+    <div class="tab">
+      i am tab
+    </div>
+    <div class="content">
+      i am content
+    </div>
   </div>
 </template>
 
