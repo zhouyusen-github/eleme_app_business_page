deea5016bb8b701e311e9ad1ccdcd2f65fbc44d4
Windcolour <1015357985@qq.com>
2020-02-01 18:37:50 +0800

    header.vue:header组件基础功能

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,8 +1,11 @@
 <template>
+  <div class="header">
+    我是header
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+export {};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
