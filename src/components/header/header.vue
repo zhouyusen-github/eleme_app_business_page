161695167ec9bb5b91f2f36bc956c3ee507f3306
Windcolour <1015357985@qq.com>
2020-02-15 16:05:28 +0800

    header.vue:实现header显示图片功能

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="header">
-    我是header
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width ="64" height="64" :src="seller.avatar"><!--src前加冒号才能加载图片，因为seller内的东西是后面传进来的-->
+        <!--src就是访问网址获取图片，反映在网页上-->
+      </div><!--头像区-->
+    </div><!--内容区-->
+    <div class="bulletin-wrapper"></div><!--公告区-->
   </div>
 </template>
 
