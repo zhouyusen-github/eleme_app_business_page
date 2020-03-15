68ae23571e8d6b7a79beadb0d8b061403d2269c3
Windcolour <1015357985@qq.com>
2020-03-16 00:01:38 +0800

    .eslintrc.js: 添加'vue/require-v-for-key': 'off'，关闭使用v-for报错:Elements in iteration expect to have 'v-bind:key' directives vue/require-v-for-key。 因为有时候没key的话，不想硬加

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'semi': ['error','always'],
     'indent':0,
-    'space-before-function-paren': 0
+    'space-before-function-paren': 0,
+    'vue/require-v-for-key': 'off'
   }
 }
