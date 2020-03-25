dcc1c5e91964d2e77aa30ebea9466f26ccd52210
Windcolour <1015357985@qq.com>
2020-03-25 19:32:07 +0800

    shopcart.vue: 购物车栏引入购物车图标

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="shopcart"></div>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper"><!--包装logo的wrapper-->
+          <div class="logo">
+            <span class="icon-shopping_cart"></span><!--引入图标字体文件，来自icon.styl-->
+          </div>
+        </div>
+      </div>
+      <div class="content-right"></div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  export default {};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
