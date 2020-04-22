e44e0da868732a2909476e0a5a5ecaa89f1d73b8
Windcolour <1015357985@qq.com>
2020-04-22 18:57:42 +0800

    food.vue:菜品详情弹出动画

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,7 +1,9 @@
 <template>
-  <div v-show="showFlag" class="food">
+  <transition name="move">
+    <div v-show="showFlag" class="food">
 
-  </div>
+    </div>
+  </transition>
 </template>
 
 <script type="text/ecmascript-6">
@@ -25,7 +27,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  .food {
+  .food
     // 为了全屏覆盖不过留下购物车的空间
     position: fixed // 生成绝对定位的元素，相对于浏览器窗口进行定位。
     left: 0
@@ -34,5 +36,7 @@
     z-index: 30 // 为了在堆叠顺序上，不覆盖购物车弹开的框
     width: 100%
     background: #fff
-  }
+    transition: all 3.5s
+    &.move-enter, &.move-leave-to
+      transform: translate3d(100%, 0, 0)
 </style>
