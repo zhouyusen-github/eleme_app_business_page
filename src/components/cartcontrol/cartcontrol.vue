7bd70f4ce9f8624188fe44fa07d49afa0495f065
Windcolour <1015357985@qq.com>
2020-04-23 15:58:03 +0800

    cartcontrol.vue:@click.stop.prevent 阻止点击穿透，保证点击按钮时不会同时点击到下面得页面

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,12 +1,12 @@
 <template>
   <div  class="cartcontrol">
     <transition name="move">
-      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
+      <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart">
         <span class="inner icon-remove_circle_outline"></span>
       </div><!--加减按钮本质是图标字体实现的-->
     </transition>
     <div class="cart-count"  v-show="food.count>0">{{food.count}}</div>
-    <div class="cart-add" @click="addCart">
+    <div class="cart-add" @click.stop.prevent="addCart">
       <span class="inner icon-add_circle"></span>
     </div>
   </div>
