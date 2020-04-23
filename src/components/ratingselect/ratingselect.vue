2ae7aa907da10dede18e66a0862b0fb57a65103a
Windcolour <1015357985@qq.com>
2020-04-23 21:02:07 +0800

    ratingselect: 实现类型选择按钮的样式（包括根据属性值变化而变化选中按钮的颜色情况）

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type"><!--选择类型的名字-->
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+    <div class="rating-type border-1px"><!--选择类型的名字-->
+      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span><!--block是统一有的样式-->
+      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
+      <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
     </div>
     <div class="switch"><!--选择是否只看内容的评价-->
       <span class=".icon-check_circle"></span>
@@ -48,5 +48,32 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import '../common/stylus/mixin';
+  .ratingselect
+    .rating-type
+      padding: 18px 0 // 用padding，而不是margin是因为要控制下面那条线长度
+      margin: 0 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      font-size: 0 // 防止block间隙
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        border-radius: 1px
+        color: rgb(77, 85, 93)
+        font-size: 12px
+        line-height: 16px
+        &.active // 被选中时的样式，共有变化
+          color: #fff
+        .count
+          font-size: 8px
+          margin-left: 2px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active // 被选中时的样式
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active // 被选中时的样式
+            background: rgb(77, 85, 93)
 </style>
