105200f6ab46108db4c8e1734cd7ea89fff70b22
Windcolour <1015357985@qq.com>
2020-04-26 13:55:46 +0800

    ratings.vue: 评价页情况概览左侧完成html+css(不难)

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -3,9 +3,46 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .ratings // 留出上面栏的空间
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview // 使用弹性布局
+      display: flex
+      padding: 18px 0
+      .overview-left
+        flex: 0 0 137px
+        padding: 6px 0
+        border-right: 1px solid rgba(7, 17 ,27, 0.1)
+        text-align: center
+        .score
+          margin-bottom: 6px
+          line-height: 28px
+          font-size:24px
+          color: rgb(255, 153, 0)
+        .title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          color:rgb(7, 17, 27)
+        .rank
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .overview-right
+        flex: 1
+        padding-left: 24px
 </style>
