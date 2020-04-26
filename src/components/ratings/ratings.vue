872d1ce416d22c8aafcab44bedf90883c20e2a56
Windcolour <1015357985@qq.com>
2020-04-27 00:01:58 +0800

    ratings.vue: 评价页情况概览左侧完成html+css

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>i am ratings</div>
+  <div class="ratings" ref="ratings">
+    <div class="rating-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+      <split></split>
+      <ratingselect :control="control" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper"><!--评论区-->
+        <ul v-show="ratings.length">
+          <li v-show="needShow(rating.rateType,rating.text)" v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img width="28px" height="28px" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommand" v-show="rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
+        </ul>
+        <div class="no-rating" v-show="!ratings.length">暂无评价</div><!--这个是负责在没有评论数据时显示的-->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
